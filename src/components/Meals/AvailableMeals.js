63032434c9d76ab3af23084b8d80d08decd63611
Tiago Gomes <tidgomes@gmail.com>
2023-03-27 09:57:10 -0300

    handle eventual fetch errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [httpError, setHttpError] = useState()
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -13,6 +14,11 @@ const AvailableMeals = () => {
       const response = await fetch(
         'https://react-academind-3054e-default-rtdb.firebaseio.com/meals.json'
       )
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -29,13 +35,24 @@ const AvailableMeals = () => {
       setIsLoading(false)
     }
 
-    fetchMeals()
+    fetchMeals().catch(error => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
 
   if (isLoading) {
     return (
       <section className={classes.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
